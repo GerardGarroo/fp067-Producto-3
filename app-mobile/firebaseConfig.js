cab77349cb1053f9de6979ab10af575c49b94f0f
Gerard Garro <geraardgarro@gmail.com>
2026-05-02 15:09:26 +0200

    actualizacion databaseURL a la región correcta y restaurar export de db

--- app-mobile/firebaseConfig.js
+++ app-mobile/firebaseConfig.js
@@ -2,13 +2,14 @@ import { initializeApp, getApps } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAFQKbBSAhnpMv2A5A8THGqhHZFmNXP6Z0",
-  authDomain: "producto-2-fp067.firebaseapp.com",
-  databaseURL: "https://producto-2-fp067-default-rtdb.firebaseio.com",
-  projectId: "producto-2-fp067",
-  storageBucket: "producto-2-fp067.firebasestorage.app",
-  messagingSenderId: "583706100820",
-  appId: "1:583706100820:web:b85c40b7261776fcf559c1"
+    apiKey: "AIzaSyAFQKbBSAhnpMv2A5A8THGqhHZFmNXP6Z0",
+    authDomain: "producto-2-fp067.firebaseapp.com",
+    databaseURL: "https://producto-2-fp067-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "producto-2-fp067",
+    storageBucket: "producto-2-fp067.firebasestorage.app",
+    messagingSenderId: "583706100820",
+    appId: "1:583706100820:web:b85c40b7261776fcf559c1",
+    measurementId: "G-GMCL1M8QHT"
 };
 
 // Asegurarnos de que Firebase se inicialice solo una vez
